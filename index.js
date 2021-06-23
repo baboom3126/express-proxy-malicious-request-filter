5aae3882303ab79fbed5b0c0461ae2174718a809
baboom3126 <km101km@gmail.com>
2021-06-23 16:45:15 +0800

    0623

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 var bodyParser = require('body-parser');
 
 var path = require('path');
 var fs = require('fs')
-var config = require(path.join(__dirname,'config.json'))
+var config = require(path.join(__dirname, 'config.json'))
 console.log(config)
 // Create Express Server
 const app = express();
@@ -14,42 +14,53 @@ const PORT = config.ProxyPort;
 const RedirectURL = config.RedirectURL;
 const MonitorPort = config.MonitorPort;
 
-fs.writeFile(path.join(__dirname,'log.txt'), '', function (err) {
-    if (err)
-        console.log(err);
-    else
-        console.log('[INFO] Create log file successfully');
-});
 
 
 
-app.use('/*',function (req,res,next){
+app.use('/*', function (req, res, next) {
     /////logger data for monitor
     var data = {}
     data.header = req.headers
     data.body = req.body
-    data.baseUrl = req.baseUrl
+    data.baseUrl = req.originalUrl
     data.method = req.method
     data.ip = req.headers['x-forwarded-for'] || (req.connection && req.connection.remoteAddress) || ''
 
     data.timestamp = new Date().getTime()
 
-    fs.appendFile(path.join(__dirname,'log.txt'),JSON.stringify(data)+"##@@\n",function(err){
-        if(err){
-            console.log(err)
-        }else{
 
-        }
-    })
     ///////check malicious request
-    if(req.body!={}){  ////////////if request post body has value
-        if(isSQLTestPass(JSON.stringify(req.body)) && isXSSTestPass(JSON.stringify(req.body))){
+    if (req.body != {}) {  ////////////if request post body has value
+
+        let isSQLTestPassFlag = isSQLTestPass(JSON.stringify(req.body))
+        let isXSSTestPassFlag = isXSSTestPass(JSON.stringify(req.body))
+
+        if ( isSQLTestPassFlag && isXSSTestPassFlag) {
+
+            writeLog(data)
+
             next()
 
-        }else{
-            console.log('[WARN] Malicious Request Detected!')
+        } else {
+            console.log('[WARN] Malicious Request Detected! Request has blocked!')
+
+            if(isSQLTestPassFlag==false){
+                console.log('[WARN] Malicious Type : SQL injection')
+                data.sqlTestPass = 'no'
+            }
+            if(isXSSTestPassFlag==false){
+                console.log('[WARN] Malicious Type : XSS attack')
+
+                data.xssTestPass = 'no'
+            }
+            writeLog(data)
+
+
         }
-    }else{             //////if request doesn't include POST body || is GET method
+    } else {             //////if request doesn't include POST body || is GET method
+
+        writeLog(data)
+
         next()
     }
 
@@ -59,26 +70,24 @@ app.use('/*',function (req,res,next){
 })
 
 
-
-var restream = function(proxyReq, req, res, options) {
+var restream = function (proxyReq, req, res, options) {
     if (req.body) {
         let bodyData = JSON.stringify(req.body);
         // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
-        proxyReq.setHeader('Content-Type','application/json');
+        proxyReq.setHeader('Content-Type', 'application/json');
         proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
         // stream the content
         proxyReq.write(bodyData);
     }
 }
-var apiProxy = createProxyMiddleware('/',  {
+var apiProxy = createProxyMiddleware('/', {
     target: RedirectURL,
-    pathRewrite: { [`^/`]: '',},
+    pathRewrite: {[`^/`]: '',},
     secure: false,
     changeOrigin: true,
     onProxyReq: restream
 });
 app.use(apiProxy);
-
 
 
 // Start Proxy
@@ -93,43 +102,77 @@ const app_monitor = express()
 app_monitor.use(express.static(path.join(__dirname, 'public')));
 
 
-app_monitor.get('/',function (req,res,next){
-    res.sendFile(path.join(__dirname,'monitor.html'))
+app_monitor.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname, 'monitor.html'))
 })
 
-app_monitor.get('/log',function (req,res,next){
-    res.sendFile(path.join(__dirname,'log.txt'))
+app_monitor.get('/setting', function (req, res, next) {
+    res.sendFile(path.join(__dirname, 'setting.html'))
 })
 
-app_monitor.listen(MonitorPort , () => {
+
+
+app_monitor.get('/log', function (req, res, next) {
+    try {
+        res.sendFile(path.join(__dirname, 'log.txt'))
+    } catch (err) {
+        res.send('')
+    }
+
+})
+
+app_monitor.post('/clearLog', function (req, res, next) {
+    try {
+        console.log('clear log')
+        fs.writeFile(path.join(__dirname, 'log.txt'),'',function (err){
+            res.send('ok')
+        })
+    } catch (err) {
+        res.send(err)
+
+    }
+
+})
+
+app_monitor.post('/getSetting',function (req,res,next){
+    try{
+        res.sendFile(path.join(__dirname, 'setting.json'))
+
+    }catch(err){
+        res.send(err)
+    }
+})
+
+
+app_monitor.listen(MonitorPort, () => {
     console.log(`Starting Monitor at port ${MonitorPort}`);
 });
 
 
-
-
-var isSQLTestPass = function (str){
+var isSQLTestPass = function (str) {
     var flag = true
 
     var regex1 = new RegExp(/(\%27)|(\')|(\-\-)|(\%23)|(#)/, 'g');
-    var regex2 = new RegExp(/((\%3D)|(=))[^\n]*((\%27)|(\')|(\-\-)|(\%3B)|(;))/,'g')
-    var regex3 = new RegExp(/\w*((\%27)|(\'))((\%6F)|o|(\%4F))((\%72)|r|(\%52))/,'g')
-    var regex4 = new RegExp(/((\%27)|(\'))union/,'g')
-    var regex5 = new RegExp(/exec(\s|\+)+(s|x)p\w+/,'g')
+    // filter the character like [ ' -- # ] and its hex equivalent. For preventing the unusual sql operation by annotating.
+    var regex2 = new RegExp(/((\%3D)|(=))[^\n]*((\%27)|(\')|(\-\-)|(\%3B)|(;))/, 'g')
+    // fliter the character like [ = ' ; -- ] and its hex equivalent. For preventing classic attack 1'or'1'='1.
+    var regex3 = new RegExp(/\w*((\%27)|(\'))((\%6F)|o|(\%4F))((\%72)|r|(\%52))/, 'g')
+    // filter the character like [ ' o O r R ]and its hex equivalent. For preventing the attack like 1'or2>1.
+    var regex4 = new RegExp(/((\%27)|(\'))union/, 'g')
+    // filter the charater like [ ' union ] and its hex equivalent. For preventing union selection attack.
+    // Similar expressions can be written for other SQL queries such as >select, insert, update, delete, drop, and so on.
+    var regex5 = new RegExp(/exec(\s|\+)+(s|x)p\w+/, 'g')
+    // filter the word like [ exec sp xp white-space ]. For protecting stored procedure
 
-    if(regex1.test(str)){
+    if (regex1.test(str)) {
         flag = false
-    }
-    else if(regex2.test(str)){
+    } else if (regex2.test(str)) {
         flag = false
-    }
-    else if(regex3.test(str)){
+    } else if (regex3.test(str)) {
         flag = false
-    }
-    else if(regex4.test(str)){
+    } else if (regex4.test(str)) {
         flag = false
-    }
-    else if(regex5.test(str)){
+    } else if (regex5.test(str)) {
         flag = false
     }
 
@@ -137,21 +180,22 @@ var isSQLTestPass = function (str){
     return flag
 }
 
-var isXSSTestPass = function (str){
+var isXSSTestPass = function (str) {
 
     var flag = true
 
-    var regex1 = new RegExp(/((\%3C)|<)((\%2F)|\/)*[a-z0-9\%]+((\%3E)|>)/,'g')
-    var regex2 = new RegExp(/((\%3C)|<)((\%69)|i|(\%49))((\%6D)|m|(\%4D))((\%67)|g|(\%47))[^\n]+((\%3E)|>)/,'g')
-    var regex3 = new RegExp(/((\%3C)|<)[^\n]+((\%3E)|>)/,'g')
+    var regex1 = new RegExp(/((\%3C)|<)((\%2F)|\/)*[a-z0-9\%]+((\%3E)|>)/, 'g')
+    // filter the chatacter like [ < / > ] and its hex equivalent. For preventing any requests contain xml element.
+    var regex2 = new RegExp(/((\%3C)|<)((\%69)|i|(\%49))((\%6D)|m|(\%4D))((\%67)|g|(\%47))[^\n]+((\%3E)|>)/, 'g')
+    // filter the character like [ < i I m M g G >] and its hex equivalent. For prevent classic attack like the injecting <img src
+    var regex3 = new RegExp(/((\%3C)|<)[^\n]+((\%3E)|>)/, 'g')
+    // filter the string start from the opening angled bracket < to closing angled bracket > through the whole request string.
 
-    if(regex1.test(str)){
+    if (regex1.test(str)) {
         flag = false
-    }
-    else if(regex2.test(str)){
+    } else if (regex2.test(str)) {
         flag = false
-    }
-    else if(regex3.test(str)){
+    } else if (regex3.test(str)) {
         flag = false
     }
 
@@ -159,3 +203,13 @@ var isXSSTestPass = function (str){
 
 }
 
+
+var writeLog = function (data){
+    fs.appendFile(path.join(__dirname, 'log.txt'), JSON.stringify(data) + "##@@\n", function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+
+        }
+    })
+}
--- public/monitor.js
+++ public/monitor.js
@@ -3,7 +3,7 @@ var data
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "/log",
+    "url": "./log",
     "method": "GET",
     "headers": {
         "content-type": "application/json",
@@ -17,19 +17,33 @@ $.ajax(settings).done(function (response) {
     data = response.split('##@@\n')
 
     var appendHtmlForTable = ``
-    for(let i = 0 ; i<data.length-1;i++){
+    for (let i = 0; i < data.length - 1; i++) {
         let temp = JSON.parse(data[i])
-        if(temp.header["user-agent"].includes("LineBotWebhook")){
-            appendHtmlForTable+=`
-                <tr onclick="show_data(${i},this)" style="background-color: #dcedc8;">
+        if (temp.header["user-agent"]) {
+            if (temp.header["user-agent"].includes("LineBotWebhook")) {
+                appendHtmlForTable += `
+               <tr onclick="show_data(${i},this)" >  <!-- style="background-color: #dcedc8;"-->
+                    <td>${(temp.sqlTestPass=='no')||(temp.xssTestPass=='no')?`<i class="material-icons" style="color:red;">close</i>`:'<i class="material-icons" style="color:green;">check</i>'}</td>
                     <td>${temp.ip} (Line)</td>
                     <td>${temp.method}</td>
                     <td>${temp.baseUrl}</td>
                     <td>${new Date(temp.timestamp).toLocaleString()}</td>
                 </tr>
         `
-        }else{
-            appendHtmlForTable+=`
+            }else{
+                appendHtmlForTable += `
+                <tr onclick="show_data(${i},this)">
+                    <td>${(temp.sqlTestPass=='no')||(temp.xssTestPass=='no')?`<i class="material-icons" style="color:red;">close</i>`:'<i class="material-icons" style="color:green;">check</i>'}</td>
+                    <td>${temp.ip}</td>
+                    <td>${temp.method}</td>
+                    <td>${temp.baseUrl}</td>
+                    <td>${new Date(temp.timestamp).toLocaleString()}</td>
+                </tr>
+        `
+            }
+
+        } else {
+            appendHtmlForTable += `
                 <tr onclick="show_data(${i},this)">
                     <td>${temp.ip}</td>
                     <td>${temp.method}</td>
@@ -39,9 +53,10 @@ $.ajax(settings).done(function (response) {
         `
         }
 
+
     }
     $('#table_body_for_requests').html(appendHtmlForTable)
-
+    $('#table_log').DataTable({responsive: true,"order": [[ 4, "desc" ]]})
     ///show the first data
     // var formatterForHeader = new JSONFormatter(JSON.parse(data[0]).header);
     // document.getElementById('div_for_header').appendChild(formatterForHeader.render());
@@ -52,19 +67,95 @@ $.ajax(settings).done(function (response) {
 });
 
 
-var show_data = function (index,that){
+var show_data = function (index, that) {
 
     $('tr').removeClass('focus')
-    $(that).addClass('focus','#eeeeee')
+    $(that).addClass('focus', '#eeeeee')
 
-    document.getElementById('div_for_header').innerHTML=""
-    document.getElementById('div_for_body').innerHTML=""
+    document.getElementById('div_for_header').innerHTML = ""
+    document.getElementById('div_for_body').innerHTML = ""
 
-    console.log()
+    var isSqlPass = JSON.parse(data[index]).sqlTestPass
+    var isXssPass = JSON.parse(data[index]).xssTestPass
+    var appenHtmlForError = ''
+    if(isSqlPass === 'no'){
+        appenHtmlForError+=`SQL injection<br>`
+    }
+    if(isXssPass === 'no'){
+        appenHtmlForError+=`Xss attack<br>`
+    }
+    if(appenHtmlForError===''){
+        appenHtmlForError="none"
+    }
+
+    $('#div_for_error').html(appenHtmlForError)
+
     var formatterForHeader = new JSONFormatter(JSON.parse(data[index]).header);
     document.getElementById('div_for_header').appendChild(formatterForHeader.render());
 
-    var formatterForBody = new JSONFormatter(JSON.parse(data[index]).body,10);
+    var formatterForBody = new JSONFormatter(JSON.parse(data[index]).body, 10);
     document.getElementById('div_for_body').appendChild(formatterForBody.render());
 
 }
+
+
+$('#btn_clear').click(function (){
+
+
+    var settings = {
+        "url": "./clearLog",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({"name":"test","email":"123@gmail.com","pwd":"123"}),
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        if(response=="ok"){
+            alert('Success')
+            location.reload()
+        }
+    });
+})
+
+
+$('#btn_export').click(function (){
+
+    if(data[0]!=""){
+    let rows = [["timestamp","ip","method","baseUrl","header","body"]];
+
+    for(let i of data){
+        rows.push([new Date((JSON.parse(data[0]).timestamp)).toJSON(),JSON.stringify(JSON.parse(data[0]).ip),JSON.stringify(JSON.parse(data[0]).method),JSON.stringify(JSON.parse(data[0]).baseUrl),JSON.stringify(JSON.parse(data[0]).header).replace(/,/g,'@'),JSON.stringify(JSON.parse(data[0]).body).replace(/,/g,'@')])
+    }
+
+    let csvContent = "data:text/csv;charset=utf-8,";
+
+    rows.forEach(function(rowArray) {
+        let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+    });
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "history_"+new Date().toJSON()+".csv");
+    document.body.appendChild(link); // Required for FF
+
+    link.click(); // This will download the data file named "my_data.csv".
+
+    }else{
+        alert('No data')
+    }
+
+
+
+
+})
+
+
+
+
+
